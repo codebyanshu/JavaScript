bb935b6c359583f2da93b87b28905fd31b14d690
hackeranshu007 <anshu290290@gmail.com>
2025-09-10 22:43:37 +0530

    Keep only Todo folder in gh-pages branch

--- java practice/playground/50Questions/Advancded/DOM/33_question/script.js
+++
@@ -1,5 +0,0 @@
-let child = document.querySelector('#child');
-
-child.addEventListener('click',(e)=>{
-    console.log(e.target.closest('.parent'));
-})
--- java practice/playground/50Questions/Advancded/DOM/34_question/script.js
+++
@@ -1,6 +0,0 @@
-let sideBar = document.querySelector('.sideBar');
-let button = document.querySelector('button');
-
-button.addEventListener('click',()=>{
-    sideBar.classList.toggle('showSideBar');
-})
